f05022f32eb913a159c82e891f149055fa5486d4
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-16 15:26:39 -0500

    mvp?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation>
+    <Route exact path="/" component={Home}>Home</Route>
+    <Route path="/about" component={About}>About</Route>
+    <Route path="/contact" component={Contact}>Contact</Route>
+    </Navigation>
+    
+
   </div>
+  
 );
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
